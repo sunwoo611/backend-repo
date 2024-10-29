4b947278c5c358864e19651f89f4f0d0b4e9d10f
sw_vi.xi <sunwoo0251@gmail.com>
2024-10-29 20:36:15 +0900

    study

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const http = require('http');
-const hostname = '127.0.0.1';
+const express = require('express');
 const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const app = express();
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug');
+app.get('/', (req, res) => res.render('index.pug'));
+app.get('/page', (req, res) => {
+    const { page } = req.query;
+    res.render('board.pug', { page });
+});    
+app.get('/posts', (req, res) => {});
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
